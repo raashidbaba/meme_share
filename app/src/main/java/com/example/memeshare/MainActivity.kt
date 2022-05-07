218de58c2680f506a1a7a08e829507cc8add5144
raashidbaba <raashidbaba849@gmail.com>
2022-05-07 22:38:05 +0530

    meme working

--- app/src/main/java/com/example/memeshare/MainActivity.kt
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -1,48 +1,87 @@
 package com.example.memeshare
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Notification
+import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
+import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.activity_main.*
-import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+    var currentImageurl:String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        LoadMeme()
+        loadMeme()
     }
-fun LoadMeme(){
-    // Instantiate the RequestQueue.
-    val queue = Volley.newRequestQueue(this)
-    val url = "https://meme-api.herokuapp.com/gimme"
+
+    fun loadMeme() {
+        progessBar.visibility = View.VISIBLE
+        // Instantiate the RequestQueue.
+   //     val queue = Volley.newRequestQueue(this)
+        val url = "https://meme-api.herokuapp.com/gimme"
 
 // Request a string response from the provided URL.
 
-    val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
             Response.Listener { response ->
-                val url = response.getString("url")
-                Glide.with(this).load(url).into(imageView)
+                // taking out url from the API
+                currentImageurl = response.getString("url")
+                Glide.with(this).load(currentImageurl).listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                       progessBar.visibility = View.GONE
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progessBar.visibility = View.GONE
+                        return false
+                    }
+                }).into(imageView)
             },
-/*    val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url,null
-            Response.Listener{ response ->
-val url = response.getString("url")
-            },*/
+
             Response.ErrorListener {
-                Toast.makeText(this,"something went wrong",Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "something went wrong", Toast.LENGTH_LONG).show()
             })
 
 // Add the request to the RequestQueue.
-    queue.add(jsonObjectRequest)
-}
-    fun shareMeme(view: View) {}
+   //     queue.add(jsonObjectRequest)
+        //following singleton pattern
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+    }
 
-    fun nextMeme(view: View) {}
+    fun shareMeme(view: View) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plane"
+        intent.putExtra(Intent.EXTRA_TEXT, "hey check out this meme $currentImageurl")
+        val chooser = Intent.createChooser(intent, "boom booom")
+        startActivity(chooser)
+    }
+
+    fun nextMeme(view: View) {
+        loadMeme()
+    }
 }
